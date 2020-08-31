9c6ab8d55ee08ff4ab4ff4608b146c35ef45e4f9
gosling <gosling@example.com>
2020-08-31 16:06:49 +0300

    Dont slide on in-between (navigation separators)

--- script.js
+++ script.js
@@ -3,22 +3,39 @@ const throttleDelay = 500;
 
 const slides = document.querySelectorAll('.page');
 
+let checkpoints = [];
+
+[...slides].forEach((slide) => {
+  checkpoints.push(slide);
+
+  if ([...slides].indexOf(slide) !== slides.length - 1) {
+    checkpoints.push(...Array(numberOfSeparators).fill('separator'));
+  }
+});
+
 let currentSlideIndex = 0;
 
-const slide = async (event) => {
+const slide = (event) => {
   event.preventDefault();
 
   const newSlide =
     event.deltaY > 0
-      ? slides[currentSlideIndex + 1] //  slide down
-      : slides[currentSlideIndex - 1]; // slide up
+      ? checkpoints[currentSlideIndex + 1] //  slide down
+      : checkpoints[currentSlideIndex - 1]; // slide up
 
-  if (![...slides].includes(newSlide)) {
+  if (![...checkpoints].includes(newSlide)) {
     return; // if the slide is not exist
   }
 
   // throttled scroll
   if (throttleTimer + throttleDelay - Date.now() < 0) {
+    if (newSlide === 'separator') {
+      currentSlideIndex =
+        event.deltaY > 0 ? currentSlideIndex + 1 : currentSlideIndex - 1;
+
+      return;
+    }
+
     newSlide.scrollIntoView({
       behavior: 'smooth',
     });
@@ -28,7 +45,7 @@ const slide = async (event) => {
     return;
   }
 
-  currentSlideIndex = [...slides].indexOf(newSlide);
+  currentSlideIndex = [...checkpoints].indexOf(newSlide);
 };
 
 window.addEventListener('wheel', () => slide(event), { passive: false });
